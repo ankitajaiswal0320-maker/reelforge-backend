beb0d92457e474d208cbd83a596b93ca9922ff02
ankitajaiswal0320-maker <ankitajaiswal0320@gmail.com>
2026-03-08 14:50:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post("/generate-video", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
